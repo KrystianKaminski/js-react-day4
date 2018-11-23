cb6c3d69dc4a0423f3a3329555d31f7ffd95e8ab
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-23 10:15:17 +0100

    Add and delete task logic

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -8,6 +8,26 @@ class ToDo extends React.Component {
         newTaskText: ''
     }
 
+    createTask = text => ({
+        taskText: text,
+        isCompleted: false,
+        // good enough to make unique key in that case
+        key: Date.now()
+    })
+
+    addTask = () => this.setState({ 
+        tasks: this.state.tasks.concat(
+            this.createTask(
+                this.state.newTaskText
+            ))
+     })
+
+     deleteTask = taskKey => this.setState({
+         tasks: this.state.tasks.filter(
+             task => task.key !== taskKey
+         )
+     })
+
     render() {
         return (
             <div>
